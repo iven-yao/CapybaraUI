688bcc59e5e8dabc90902f349025b611513efb14
ivenyao <ivenyao820109@gmail.com>
2024-08-15 02:39:50 -0700

    update vite config for deploying to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true
-  },
-  build: {
-    outDir: 'build'
-  },
-  resolve: {
-    alias: {
-        src: "/src",
+    base: "/capybaraui",
+    plugins: [react()],
+    server: {
+        open: true
+    },
+    build: {
+        outDir: 'build'
+    },
+    resolve: {
+        alias: {
+            src: "/src",
+        }
     }
-  }
 })
